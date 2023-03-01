628449c58cccbc9c803c3bcd0ae91ed547c8645c
Jose <alu0101441702@ull.edu.es>
2023-03-01 20:19:14 +0000

    Script para covering

--- test/data/logged2.js
+++
@@ -1,9 +0,0 @@
-function foo(a, b) {
-    console.log('Entering foo()');
-    var x = 'blah';
-    var y = function (z) {
-        console.log('Entering <anonymous function>()');
-        return z + 3;
-    }(2);
-}
-foo(1, 'wut', 3);
