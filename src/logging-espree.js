cfe271995dc1d944aa72dcdb669e3ffbe3327b23
Jose <alu0101441702@ull.edu.es>
2023-03-02 17:44:21 +0000

    Documentation

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -3,6 +3,13 @@ import * as espree from "espree";
 import * as estraverse from "estraverse";
 import * as fs from "fs/promises";
 
+/**
+ * @desc Transpiler for JavaScript, adds console.log at the beggining of functions
+ * @param {File} inputFile File with JS code
+ * @param {File} outputFile 
+ * @returns outputFile with the code including console.log in functions, if no
+ * outputFile was specificated it will print the output
+ */
 export async function transpile(inputFile, outputFile) {
   let input = await fs.readFile(inputFile, 'utf-8')
   let output = addLogging(input);
@@ -13,6 +20,12 @@ export async function transpile(inputFile, outputFile) {
   await fs.writeFile(outputFile, output)
 }
 
+/**
+ * @desc Takes code as a string and returns an AST wiht the console.log's added
+ * at the start of functions
+ * @param {string} code 
+ * @returns AST of the code with the console.log's added
+ */
 export function addLogging(code) {
   const ast = espree.parse(code, { ecmaVersion: 12, loc: true }); // attach line/column location information to each node https://github.com/eslint/espree
   estraverse.traverse(ast, {
@@ -27,6 +40,10 @@ export function addLogging(code) {
   return escodegen.generate(ast);
 }
 
+/**
+ * @desc Adds console.log to the node of the function (passed by reference because its an object)
+ * @param {object} node AST node of a function 
+ */
 function addBeforeCode(node) {
   const name = node.id ? node.id.name : '<anonymous function>';
   let parmNames = "";
